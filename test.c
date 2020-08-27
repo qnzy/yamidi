f6704127dbfa9f90e75928570e0fc820b1f28e84
Yves Kunz <yves.kunz@gmail.com>
2020-08-27 22:35:20 +0200

    add printMsg function

--- test.c
+++ test.c
@@ -2,6 +2,52 @@
 #define YAMIDI_IMPLEMENTATION
 #include "yamidi.h"
 
+void printMsg(struct midiMsg_s msg) {
+    switch(msg.status) {
+        case midiStatusNoteOff:
+            printf("Note Off, chan=%d, key=%d, vel=%d\n", 
+                    msg.params.midiParamNoteOff.chan,
+                    msg.params.midiParamNoteOff.key,
+                    msg.params.midiParamNoteOff.vel);
+            break;
+        case midiStatusNoteOn:
+            printf("Note On, chan=%d, key=%d, vel=%d\n", 
+                    msg.params.midiParamNoteOn.chan,
+                    msg.params.midiParamNoteOn.key,
+                    msg.params.midiParamNoteOn.vel);
+            break;
+        case midiStatusPolyAT:
+            printf("PolyAT, chan=%d, key=%d, vel=%d\n", 
+                    msg.params.midiParamPolyAT.chan,
+                    msg.params.midiParamPolyAT.key,
+                    msg.params.midiParamPolyAT.vel);
+            break;
+        case midiStatusCC:
+            printf("CC, chan=%d, ctrl=%d, vll=%d\n", 
+                    msg.params.midiParamCC.chan,
+                    msg.params.midiParamCC.ctrl,
+                    msg.params.midiParamCC.val);
+            break;
+        case midiStatusProgramChange:
+            printf("ProgramChange, chan=%d, prog=%d\n", 
+                    msg.params.midiParamProgramChange.chan,
+                    msg.params.midiParamProgramChange.prog);
+            break;
+        case midiStatusChannelAT:
+            printf("ChannelAT, chan=%d, vel=%d\n", 
+                    msg.params.midiParamChannelAT.chan,
+                    msg.params.midiParamChannelAT.vel);
+            break;
+        case midiStatusPitchBend:
+            printf("Pitch Bend, chan=%d, val=%d\n", 
+                    msg.params.midiParamPitchBend.chan,
+                    msg.params.midiParamPitchBend.val);
+            break;
+        default:
+            printf("unknown midi msg\n");
+    }
+}
+
 int main() {
     struct midiBytes_s raw;
     raw = genMidiMessage(
@@ -19,7 +65,7 @@ int main() {
     if (parseMidiByte(0x85, &msg, &state)) {printf("error on line %d\n", __LINE__);}
     if (parseMidiByte(0x01, &msg, &state)) {printf("error on line %d\n", __LINE__);}
     if (!parseMidiByte(0x02, &msg, &state)) {printf("error on line %d\n", __LINE__);}
-    printf("msg.status = %x\n",msg.status);
+    printMsg(msg);
 
     return 0;
 }
